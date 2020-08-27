09759b1b94e1167b33f2264c3a192c90729c7d3e
Bala M <balamony.m@gmail.com>
2020-08-27 16:23:35 +0530

    Stash changes and then update the hello world with new LOC

--- git_basics/HelloWorld.java
+++ git_basics/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World to Git with staged");
+        System.out.println("Hello I have been done after stashing some changes");
     }
 }
